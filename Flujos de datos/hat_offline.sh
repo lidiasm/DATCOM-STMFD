54173ca2cf5ac848c3e8cada8c9d69a33f196182
Lidia Sánchez <lidia.sm.96@gmail.com>
2022-04-17 00:26:20 +0200

    MOA scripts para entrenamiento offline con Árboles de Decisión

---
+++ Flujos de datos/hat_offline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for seed in $(seq 30)
+do
+    echo $seed
+    java -cp moa.jar -javaagent:sizeofag-1.0.4.jar moa.DoTask \
+    "EvaluateModel -m (
+        LearnModel -l trees.HoeffdingAdaptiveTree
+        -s (generators.WaveformGenerator -i $seed) -m 100000) 
+        -s (generators.WaveformGenerator -i 26) -i 100000
+    " > "./hat_offline_results/$seed.csv"
+done
